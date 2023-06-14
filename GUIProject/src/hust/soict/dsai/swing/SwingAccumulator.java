584e57fcf28caee7fb515fe571942c308168d1a8
Chinh Tran <93480420+relatable52@users.noreply.github.com>
2023-06-14 11:45:12 +0700

    Create SwingAccumulator.java

---
+++ GUIProject/src/hust/soict/dsai/swing/SwingAccumulator.java
@@ -0,0 +1,14 @@
+package hust.soict.dsai.swing;
+
+public class SwingAccumulator {
+
+	public SwingAccumulator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
