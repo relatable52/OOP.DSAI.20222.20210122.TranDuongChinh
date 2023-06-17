59cd15cb7277f1ca959dccf6f6b0a91109040018
Chinh Tran <93480420+relatable52@users.noreply.github.com>
2023-06-18 02:09:52 +0700

    Update SwingAccumulator.java

--- GUIProject/src/hust/soict/dsai/swing/SwingAccumulator.java
+++ GUIProject/src/hust/soict/dsai/swing/SwingAccumulator.java
@@ -1,19 +1,49 @@
 package hust.soict.dsai.swing;
 
+import java.awt.*;
+import java.awt.event.*;
+
 import javax.swing.*;
 
 public class SwingAccumulator extends JFrame {
+
 	private JTextField tfInput;
 	private JTextField tfOutput;
 	private int sum = 0;
 
 	public SwingAccumulator() {
-		// TODO Auto-generated constructor stub
+		Container cp = getContentPane();
+		cp.setLayout(new GridLayout(2, 2));
+		
+		cp.add(new JLabel("Enter an Integer: "));
+		
+		tfInput = new JTextField(10);
+		cp.add(tfInput);
+		tfInput.addActionListener(new TFInputListener());
+		
+		cp.add(new JLabel("The Accumulated Sum is: "));
+		
+		tfOutput = new JTextField(10);
+		tfOutput.setEditable(false);
+		cp.add(tfOutput);
+		
+		setTitle("Swing Accumulator");
+		setSize(350, 120);
+		setVisible(true);
+	}
+	
+	private class TFInputListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			int numberIn = Integer.parseInt(tfInput.getText());
+			sum += numberIn;
+			tfInput.setText("");
+			tfOutput.setText(sum + "");
+		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new SwingAccumulator();
 	}
 
 }
