684e3a4d52a86c204c2cfa6386e8ef8c9d04e91e
Chinh Tran <93480420+relatable52@users.noreply.github.com>
2023-06-25 21:21:47 +0700

    JavaFX

--- GUIProject/src/hust/soict/dsai/javafx/PaintController.java
+++ GUIProject/src/hust/soict/dsai/javafx/PaintController.java
@@ -1,23 +1,29 @@
 package hust.soict.dsai.javafx;
 
 import javafx.event.*;
-import javafx.fxml.FXML;
+import javafx.fxml.*;
 import javafx.scene.input.*;
-import javafx.scene.layout.VBox;
+import javafx.scene.paint.*;
+import javafx.scene.shape.*;
+import javafx.scene.layout.*;
 
 public class PaintController {
 
     @FXML // fx:id="drawingAreaPane"
-    private VBox drawingAreaPane; // Value injected by FXMLLoader
+    private Pane drawingAreaPane; // Value injected by FXMLLoader
+    
+    @FXML
+    private VBox controlPane;
 
     @FXML
     void clearButtonPressed(ActionEvent event) {
-
+    	drawingAreaPane.getChildren().clear();
     }
 
     @FXML
     void drawingAreaMouseDragged(MouseEvent event) {
-
+    	Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
+    	drawingAreaPane.getChildren().add(newCircle);
     }
 
 }
