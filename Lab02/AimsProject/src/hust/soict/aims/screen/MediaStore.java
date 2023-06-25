52041fa4849df6f066d029514dfaf0aa69934498
Chinh Tran <93480420+relatable52@users.noreply.github.com>
2023-06-25 17:25:44 +0700

    Added StoreScreen

---
+++ Lab02/AimsProject/src/hust/soict/aims/screen/MediaStore.java
@@ -0,0 +1,62 @@
+package hust.soict.aims.screen;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.border.*;
+
+import hust.soict.aims.media.*;
+import hust.soict.aims.cart.*;
+
+public class MediaStore extends JPanel{
+	private Media media;
+	private Cart cart;
+	public MediaStore(Media media, Cart cart) {
+		this.media = media;
+		this.cart = cart;
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
+		JLabel title = new JLabel(media.getTitle());
+		title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 20));
+		title.setAlignmentX(CENTER_ALIGNMENT);
+		
+		JLabel cost = new JLabel(media.getCost()+ " $");
+		cost.setAlignmentX(CENTER_ALIGNMENT);
+		
+		JPanel container = new JPanel();
+		container.setLayout(new FlowLayout(FlowLayout.CENTER));
+		
+		ActionListener playMedia = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Playing " +"\"" + media.getTitle() + "\"");
+			}
+		};
+		
+		ActionListener addMedia = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				cart.addMedia(media);
+				JOptionPane.showMessageDialog(null, "Media added\n" + "Total number in cart: " + cart.getItemsOrdered().size());
+			}
+		};
+		
+		JButton add = new JButton("Add to cart");
+		add.addActionListener(addMedia);
+		container.add(add);
+		
+		if(media instanceof Playable) {
+			JButton play = new JButton("Play");
+			play.addActionListener(playMedia);
+			container.add(play);
+		}
+		
+		this.add(Box.createVerticalGlue());
+		this.add(title);
+		this.add(cost);
+		this.add(Box.createVerticalGlue());
+		this.add(container);
+		
+		this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	}
+}
--- Lab02/AimsProject/src/hust/soict/aims/store/Store.java
+++ Lab02/AimsProject/src/hust/soict/aims/store/Store.java
@@ -6,7 +6,24 @@ import java.util.*;
 public class Store {
 	private ArrayList<Media> ItemsInStore = new ArrayList<Media>();
 	
-	public Store() {}
+	public Store() {
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		this.ItemsInStore.add(dvd1);
+		this.ItemsInStore.add(dvd1);
+		this.ItemsInStore.add(dvd1);
+		
+		Book book1 = new Book("Calculus 1");
+		this.ItemsInStore.add(book1);
+		this.ItemsInStore.add(book1);
+		this.ItemsInStore.add(book1);
+		this.ItemsInStore.add(book1);
+		
+		CompactDisc cd1 = new CompactDisc("Algebra");
+		this.ItemsInStore.add(cd1);
+		this.ItemsInStore.add(cd1);
+		this.ItemsInStore.add(cd1);
+
+	}
 	
 	public ArrayList<Media> getItemsInStore() {
 		return this.ItemsInStore;
