617d347a6c2407efc08ea8ff7895b053b9bc5c3a
Chinh Tran <93480420+relatable52@users.noreply.github.com>
2023-06-25 18:08:38 +0700

    Javafx

---
+++ GUIProject/src/hust/soict/dsai/javafx/PaintController.java
@@ -0,0 +1,23 @@
+package hust.soict.dsai.javafx;
+
+import javafx.event.*;
+import javafx.fxml.FXML;
+import javafx.scene.input.*;
+import javafx.scene.layout.VBox;
+
+public class PaintController {
+
+    @FXML // fx:id="drawingAreaPane"
+    private VBox drawingAreaPane; // Value injected by FXMLLoader
+
+    @FXML
+    void clearButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void drawingAreaMouseDragged(MouseEvent event) {
+
+    }
+
+}
